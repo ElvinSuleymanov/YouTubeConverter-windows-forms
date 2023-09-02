c572ea212f8ca374d4fb3500d6bd179754537d92
ElvinSuleymanov <elvinsuleymanov150@gmail.com>
2023-09-02 21:14:47 +0400

    v1

---
+++ YoutubeConverter/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace YoutubeConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(144, 276);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(206, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Download MP4";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(250, 52);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(5);
+            this.label1.Size = new System.Drawing.Size(286, 42);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Youtube URL";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(125, 169);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(516, 34);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 395);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(417, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 46);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Download MP3";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(275, 377);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(218, 23);
+            this.progressBar1.TabIndex = 8;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ YoutubeConverter/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeExplode;
+using YoutubeConverter;
+using AngleSharp;
+using System.Runtime.CompilerServices;
+using System.IO;
+using YoutubeExplode.Videos.Streams;
+
+namespace YoutubeConverter
+{
+    public partial class Form1 : Form
+    {
+        public string desktopPath { get; set; }
+        public YoutubeClient YoutubeClient { get; set; }
+        public string inputValue { get; set; }
+
+
+        DirectoryInfo dirInfo = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            YoutubeClient = new YoutubeClient();
+
+            progressBar1.Hide();
+
+            desktopPath = Path.Combine(dirInfo.FullName,"Youtube Downloads");
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+
+        {
+        }
+
+      
+        private async void button2_Click(object sender, EventArgs e)
+        {
+
+           bool existance =  Directory.Exists(Path.Combine(dirInfo.FullName,"Youtube Downloads"));
+
+           dirInfo.CreateSubdirectory("Youtube Downloads");
+
+
+            var videoManifest = await YoutubeClient.Videos.Streams.GetManifestAsync(inputValue);
+
+            var video = videoManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+
+            var videoInfo = await YoutubeClient.Videos.GetAsync(inputValue);
+
+            //await YoutubeClient.Videos.Streams.DownloadAsync(targetVideo, desktopPath + $"\\{video.Title}.{targetVideo.Container}");
+
+            string newVideo = $"{videoInfo.Title}.{video.Container}";
+
+            label2.AutoSize = true;
+            string desktopTarget = desktopPath + $"\\{newVideo}";
+
+            label2.Text = desktopTarget;
+
+            //FileStream fs = new FileStream(desktopTarget,FileMode.Open);
+
+            var stream = YoutubeClient.Videos.Streams.GetAsync(video);
+
+            // var res  = await stream.AsTask();
+
+            //byte[] videoBytes = new byte[1000000];
+
+            //res.Write(videoBytes, 0, videoBytes.Length);
+
+            //res.Close();
+
+            char[] invalidChars = Path.GetInvalidPathChars();
+
+            var filteredTarget = desktopTarget.ToCharArray().Where<char>((s) => {
+
+               if (invalidChars.Contains(s))
+                {
+                    return false;
+                }
+               else
+                {
+                    return true;
+                }
+            });
+
+
+
+
+            string newww = new string(filteredTarget.ToArray());
+
+            await YoutubeClient.Videos.Streams.DownloadAsync(video,newww);
+
+          
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            inputValue = richTextBox1.Text;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        private async void button1_Click_2(object sender, EventArgs e)
+        {
+
+            bool existance = Directory.Exists(Path.Combine(dirInfo.FullName, "Youtube Downloads"));
+
+            dirInfo.CreateSubdirectory("Youtube Downloads");
+
+
+            var videoManifest = await YoutubeClient.Videos.Streams.GetManifestAsync(inputValue);
+
+            var video = videoManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+
+            var videoInfo = await YoutubeClient.Videos.GetAsync(inputValue);
+
+            //await YoutubeClient.Videos.Streams.DownloadAsync(targetVideo, desktopPath + $"\\{video.Title}.{targetVideo.Container}");
+
+            string newVideo = $"{videoInfo.Title}.{video.Container}";
+
+            label2.AutoSize = true;
+            string desktopTarget = desktopPath + $"\\{newVideo}";
+
+            label2.Text = desktopTarget;
+
+            //FileStream fs = new FileStream(desktopTarget,FileMode.Open);
+
+            var stream = YoutubeClient.Videos.Streams.GetAsync(video);
+
+            
+            // var res  = await stream.AsTask();
+
+            //byte[] videoBytes = new byte[1000000];
+
+            //res.Write(videoBytes, 0, videoBytes.Length);
+
+            //res.Close();
+
+            char[] invalidChars = Path.GetInvalidPathChars();
+
+            var filteredTarget = desktopTarget.ToCharArray().Where<char>((s) => {
+
+                if (invalidChars.Contains(s))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            });
+
+
+
+
+            string newww = new string(filteredTarget.ToArray());
+
+            await YoutubeClient.Videos.Streams.DownloadAsync(video, newww);
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
